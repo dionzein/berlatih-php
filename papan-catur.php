16cefcf9aa2b9c87d48a0fb270497bf68b31a041
dionzein <dion.zein@ui.ac.id>
2020-07-30 05:54:42 +0700

    tugas git laravel

---
+++ papan-catur.php
@@ -0,0 +1,34 @@
+<!-- 2. Papan Catur
+Buatlah sebuah file dengan nama papan-catur.php.
+Tulislah di dalam file tersebut sebuah function dengan nama papan_catur yang menerima parameter angka.
+function tersebut akan mereturn string yang membentuk sebuah papan catur dengan simbol pagar “#” dengan ukuran angka x angka. -->
+<?php
+function papan_catur($angka) {
+// tulis kode di sini
+}
+// TEST CASES
+echo papan_catur(4); 
+/*
+# # # #
+ # # #
+# # # #
+ # # #
+ */
+echo papan_catur(8);
+/* 
+# # # # # # # #
+ # # # # # # # 
+# # # # # # # #
+ # # # # # # # 
+# # # # # # # #
+ # # # # # # #
+# # # # # # # #
+ # # # # # # #
+echo papan_catur(5) 
+/*
+# # # # #
+ # # # #
+# # # # #
+ # # # # 
+# # # # #
+*/
